0cf9438eb66758be0668fb75edec3ca4e87668a4
ngroggy <ngrogg@ethz.ch>
2024-11-24 12:20:49 +0100

    [refact] relocate output folder generation to dataloader

--- utils/general/dataloader.py
+++ utils/general/dataloader.py
@@ -1,6 +1,7 @@
 import json
 import os
 from PIL import Image, ImageEnhance
+from pathlib import Path
 
 def delete_files(*file_paths):
     for file_path in file_paths:
@@ -46,3 +47,20 @@ def load_image(img_path, bbox, saturation=1.0, contrast=1.0, sharpness=1.0, crop
     image_black.paste(image_crop, (bbox["xmin"], bbox["ymin"]))
 
     return image, image_crop, image_black
+
+def create_output_dir(rerun, output_dir):
+
+    output_dir = Path(output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+    
+    output_dir_length = len(os.listdir(output_dir))
+
+    if rerun is True:
+        output_dir = Path(os.path.join(output_dir, f"{output_dir_length:02d}"))
+    else:
+        output_dir = Path(os.path.join(output_dir, f"{output_dir_length + 1:02d}"))
+
+    for key in ["full", "crop", "black"]:
+        os.makedirs(os.path.join(output_dir, key), exist_ok=True)
+    
+    return output_dir
